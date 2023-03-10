292289c078b53082429162e3ab3173199af40627
RusakFroog <sg970465@gmail.com>
2023-03-10 23:29:22 +0100

    Added image for guns +added new method for client side, commited a list, with max. 15 duels

--- duels.js
+++ duels.js
@@ -1,6 +1,7 @@
 var duels = new Vue({
     el: ".window",
     data: {
+        duelsCreated: [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false  ],
         style: 0,
         offName: "Carabine",
         nameGun: "carabine",
@@ -11,8 +12,14 @@ var duels = new Vue({
             this.offName = offname;
             this.nameGun = name;
         },
+        exitWindow(){
+            mp.trigger("client_exitWindow");
+        },
         createMatch(){
             mp.trigger("client_createDuel", this.nameGun, this.price);
-        }
+        },
+        joinToMatch(){
+            mp.trigger("client_joinDuel");
+        },
     }
 });
